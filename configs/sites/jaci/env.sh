7a640a515ee728b2808067bd47b07dadebdba42d
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-04-28 22:51:51 -0300

    configs: update default JACI queue

--- configs/sites/jaci/env.sh
+++ configs/sites/jaci/env.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-# Runtime defaults for JACI-like PBS environments.
-# Review all values on the target machine before operational use.
+# Runtime defaults for JACI PBS environments.
+# Values are based on the queue list observed on JACI and should be reviewed before operational use.
 
 export KSA_SITE_NAME="jaci"
-export KSA_PBS_QUEUE="workq"
+export KSA_PBS_QUEUE="pesqmini"
 export KSA_PBS_ACCOUNT="CHANGE_ME"
 export KSA_WALLTIME="00:30:00"
 export KSA_NODES="1"
 export KSA_NCPUS="1"
 export KSA_MEM="4gb"
 export KSA_OMP_NUM_THREADS="1"
+
+# Observed enabled queues include: aux, pesqextra, pesqhigh, pesqmidi, pesqmini, oper, preoper and longtime.
+# The queue workq was observed as disabled and should not be used as the default.
